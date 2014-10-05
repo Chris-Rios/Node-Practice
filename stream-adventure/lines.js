489d8b0061c0753aaa7641f91ee1e4148ba383d7
chris-rios <rioschm@gmail.com>
2014-10-05 00:50:31 -0400

    Add more comments to lines.js

--- stream-adventure/lines.js
+++ stream-adventure/lines.js
@@ -3,16 +3,16 @@ var through = require("through");
 var split = require("split");
 var counter = 1;
 
-process.stdin
-	.pipe(split())
+process.stdin  //takes input from stdin
+	.pipe(split())  //splits input
 	.pipe(through(function(line){
 		if(counter%2!==0){    //check if an even line is being outputted
-			this.queue(line.toString().toLowerCase()+'\n');
+			this.queue(line.toString().toLowerCase()+'\n'); 
 		}
 		else{
 			this.queue(line.toString().toUpperCase()+'\n');
 		}
 		counter++;
-	},function(){
+	},function(){  //fills queue with null when input is finished
 		this.queue(null);
-	})).pipe(process.stdout);
+	})).pipe(process.stdout); //pipes to stdout
